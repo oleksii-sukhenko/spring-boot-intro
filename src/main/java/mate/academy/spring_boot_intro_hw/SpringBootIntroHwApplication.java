214f95c36e565525defff7fa8b71b53f75a933cc
Oleksii <sukhenko.oleksii@gmail.com>
2024-08-23 23:03:01 +0300

    create spring

---
+++ src/main/java/mate/academy/spring_boot_intro_hw/SpringBootIntroHwApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.spring_boot_intro_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntroHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mate/academy/spring_boot_intro_hw/SpringBootIntroHwApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.spring_boot_intro_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntroHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
