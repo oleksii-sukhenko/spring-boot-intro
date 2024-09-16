f82eed227e65513645d112ca564f7905656b7976
Oleksii <sukhenko.oleksii@gmail.com>
2024-09-16 23:29:06 +0300

    add user entity + mvn clean package

--- src/main/java/mate/academy/validate/FieldMatchValidator.java
+++ src/main/java/mate/academy/validate/FieldMatchValidator.java
@@ -2,9 +2,8 @@ package mate.academy.validate;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.springframework.beans.BeanWrapperImpl;
-
 import java.util.Objects;
+import org.springframework.beans.BeanWrapperImpl;
 
 public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
     private String firstField;
