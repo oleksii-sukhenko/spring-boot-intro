482fbae8e896fe846248b897730f23274cf35fcc
Oleksii <sukhenko.oleksii@gmail.com>
2024-09-19 22:58:02 +0300

    add security and roles

--- src/main/java/mate/academy/service/user/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/service/user/impl/UserServiceImpl.java
@@ -5,16 +5,24 @@ import mate.academy.dto.user.UserRegistrationRequestDto;
 import mate.academy.dto.user.UserResponseDto;
 import mate.academy.exception.RegistrationException;
 import mate.academy.mapper.UserMapper;
+import mate.academy.model.Role;
+import mate.academy.model.Role.RoleName;
 import mate.academy.model.User;
+import mate.academy.repository.role.RoleRepository;
 import mate.academy.repository.user.UserRepository;
 import mate.academy.service.user.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -22,7 +30,10 @@ public class UserServiceImpl implements UserService {
         if (userRepository.existsByEmail(requestDto.getEmail())) {
             throw new RegistrationException("This email is already taken");
         }
-        User user = userRepository.save(userMapper.toModel(requestDto));
-        return userMapper.toDto(user);
+        User user = userMapper.toModel(requestDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Role role = roleRepository.findByRole(RoleName.ROLE_USER);
+        user.setRoles(Set.of(role));
+        return userMapper.toDto(userRepository.save(user));
     }
 }
