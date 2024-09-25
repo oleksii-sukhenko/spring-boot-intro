9359057a01b3db54c5119cd8e99cd6d5295f3a92
Oleksii <sukhenko.oleksii@gmail.com>
2024-09-25 19:12:33 +0300

    Update Category.java

--- src/main/java/mate/academy/model/Category.java
+++ src/main/java/mate/academy/model/Category.java
@@ -7,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
@@ -18,7 +17,6 @@ import org.hibernate.annotations.SQLRestriction;
 @Setter
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
 @SQLRestriction(value = "is_deleted=false")
-@NoArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
