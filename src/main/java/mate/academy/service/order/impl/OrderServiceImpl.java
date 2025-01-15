e65896a47461840eaa14995cc5cdd3fec88347f8
Oleksii <sukhenko.oleksii@gmail.com>
2025-01-15 22:29:11 +0200

    after Artur's review

--- src/main/java/mate/academy/service/order/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/order/impl/OrderServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import mate.academy.dto.order.OrderRequestDto;
 import mate.academy.dto.order.OrderResponseDto;
