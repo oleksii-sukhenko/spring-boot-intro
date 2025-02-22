8b266ad6bcc16c055d8ba233247ca2002e90cc74
Oleksii <sukhenko.oleksii@gmail.com>
2025-02-22 11:44:52 +0200

    mvn clean package refactor

--- src/test/resources/database/book/book-data.sql
+++ src/test/resources/database/book/book-data.sql
@@ -1,11 +1,11 @@
-INSERT INTO books (title, author, isbn, price, description, cover_image, is_deleted)
-VALUES  ('First book', 'First Author', 'first_isbn', 1.11, 'First description', 'first_image.img', 0),
-        ('Second book', 'Second Author', 'second_isbn', 2.22, 'Second description', 'second_image.img', 0),
-        ('Third book', 'Third Author', 'third_isbn', 3.33, 'Third description', 'third_image.img', 0);
+INSERT INTO books (id, title, author, isbn, price, description, cover_image, is_deleted)
+VALUES  (1, 'First book', 'First Author', 'first_isbn', 1.11, 'First description', 'first_image.img', 0),
+        (2, 'Second book', 'Second Author', 'second_isbn', 2.22, 'Second description', 'second_image.img', 0),
+        (3, 'Third book', 'Third Author', 'third_isbn', 3.33, 'Third description', 'third_image.img', 0);
 
-INSERT INTO categories (name, description, is_deleted)
-VALUES  ('first category', 'first category', 0),
-        ('second category', 'second category', 0);
+INSERT INTO categories (id, name, description, is_deleted)
+VALUES  (1, 'first category', 'first category', 0),
+        (2, 'second category', 'second category', 0);
 
 INSERT INTO books_categories (book_id, category_id)
 VALUES  (1, 1),
