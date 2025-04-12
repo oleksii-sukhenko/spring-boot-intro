4727c02967222139175a98940f9b4c9e427f4597
Oleksii <sukhenko.oleksii@gmail.com>
2025-04-12 22:09:40 +0300

    Add names for OrderController and AuthenticationController

--- src/main/java/mate/academy/controller/OrderController.java
+++ src/main/java/mate/academy/controller/OrderController.java
@@ -1,5 +1,7 @@
 package mate.academy.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Order management", description = "Endpoints for managing orders")
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
@@ -32,6 +35,7 @@ public class OrderController {
 
     @PostMapping
     @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "Place order", description = "Place order for authenticated user")
     public ResponseEntity<OrderResponseDto> placeOrder(
             @RequestBody @Valid OrderRequestDto request,
             @AuthenticationPrincipal User user) {
@@ -40,6 +44,10 @@ public class OrderController {
 
     @GetMapping
     @PreAuthorize("hasRole('USER')")
+    @Operation(
+            summary = "Watch order history",
+            description = "Watch order history for authenticated user"
+    )
     public ResponseEntity<Page<OrderResponseDto>> getOrderHistory(
             @AuthenticationPrincipal User user,
             @PageableDefault(sort = "orderDate", direction = Sort.Direction.DESC)
@@ -49,6 +57,10 @@ public class OrderController {
 
     @PatchMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(
+            summary = "Update order status",
+            description = "Update order using orderId (only for admin role)"
+    )
     public ResponseEntity<OrderResponseDto> updateOrderStatus(
             @PathVariable Long id,
             @RequestBody @Valid OrderUpdateRequestDto request) {
@@ -57,12 +69,17 @@ public class OrderController {
 
     @GetMapping("/{orderId}/items")
     @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "Get orderItems", description = "Get orderItems using orderId")
     public ResponseEntity<List<OrderItemResponseDto>> getOrderItems(@PathVariable Long orderId) {
         return ResponseEntity.ok(orderService.getOrderItems(orderId));
     }
 
     @GetMapping("/{orderId}/items/{itemId}")
     @PreAuthorize("hasRole('USER')")
+    @Operation(
+            summary = "Get orderItem",
+            description = "Get orderItem using orderId and itemId"
+    )
     public ResponseEntity<OrderItemResponseDto> getOrderItem(
             @PathVariable Long orderId,
             @PathVariable Long itemId) {
